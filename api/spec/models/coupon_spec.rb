ee68f14e407360f5f31b5b7eeaba610079e4e7e7
Rian Dos Santos <riannegreiros@gmail.com>
2023-03-29 16:59:52 -0300

    add due date custom validation to coupon

--- api/spec/models/coupon_spec.rb
+++ api/spec/models/coupon_spec.rb
@@ -11,4 +11,22 @@ RSpec.describe Coupon, type: :model do
   it { should validate_numericality_of(:discount_value).is_greater_than(0)}
 
   it { should validate_presence_of(:due_date) }
+
+  it "should not allow due date in the past" do
+    subject.due_date = Date.yesterday
+    subject.valid?
+    expect(subject.errors[:due_date]).to include("Must be a future date")
+  end
+
+  it "should not allow current date" do
+    subject.due_date = Date.today
+    subject.valid?
+    expect(subject.errors[:due_date]).to include("Must be a future date")
+  end
+
+  it "should allow future date" do
+    subject.due_date = Date.tomorrow
+    subject.valid?
+    expect(subject.errors[:due_date]).to_not include("Must be a future date")
+  end
 end
