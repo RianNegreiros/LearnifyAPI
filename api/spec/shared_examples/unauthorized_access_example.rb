881a1f019d39fc2e2c3ebc78badf41c6bfd5a0e0
Rian Dos Santos <riannegreiros@gmail.com>
2023-03-31 13:45:09 -0300

    fix name

--- api/spec/shared_examples/unauthorized_access_example.rb
+++
@@ -1,9 +0,0 @@
-shared_examples "unauthorized access" do
-  it "returns unauthorized error" do
-    expect(body_json['errors']['message']).to eq "Unauthorized access"
-  end
-
-  it "returns unauthorized status" do
-    expect(response).to have_http_status(:unauthorized)
-  end
-end
