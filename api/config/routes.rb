6835dc13848372679ef2b0c52bd258380d24ed84
Rian Dos Santos <riannegreiros@gmail.com>
2023-03-31 13:45:19 -0300

    coupons CRUD

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth/v1/user'
 
-  namespace :admin do
+  namespace :admin, defaults: { format: :json } do
     namespace :v1 do
       get "home" => "home#index"
       resources :categories
+      resources :coupons
     end
   end
 
   namespace :front do
     namespace :v1 do
-      
     end
   end
 end
