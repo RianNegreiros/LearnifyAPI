9870f1a0b27dfbe0ea8926d7545464c719b21b35
Rian Dos Santos <riannegreiros@gmail.com>
2023-04-03 15:02:10 -0300

    refactor

--- api/spec/services/admin/model_loading_service_spec.rb
+++ api/spec/services/admin/model_loading_service_spec.rb
@@ -1,13 +1,13 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe Admin::ModelLoadingService do
-  context "#call" do
+  context "when #call" do
     let!(:categories) { create_list(:category, 15) }
-
-    context "when params are passed" do
+    
+    context "when params are present" do
       let!(:search_categories) do
         categories = []
-        15.times { |i| categories << create(:category, name: "Search #{i + 1}") }
+        15.times { |n| categories << create(:category, name: "Search #{n + 1}") }
         categories
       end
 
@@ -15,14 +15,14 @@ describe Admin::ModelLoadingService do
         { search: { name: "Search" }, order: { name: :desc }, page: 2, length: 4 }
       end
 
-      it "returns correct :length pagination" do
+      it "returns right :length following pagination" do
         service = described_class.new(Category.all, params)
         result_categories = service.call
         expect(result_categories.count).to eq 4
       end
 
-      it "returns records with correct search, order and pagination" do
-        search_categories.sort! { |a, b| b.name <=> a.name }
+      it "returns records following search, order and pagination" do
+        search_categories.sort! { |a, b| b[:name] <=> a[:name] }
         service = described_class.new(Category.all, params)
         result_categories = service.call
         expected_categories = search_categories[4..7]
@@ -30,14 +30,14 @@ describe Admin::ModelLoadingService do
       end
     end
 
-    context "when params not are passed" do
-      it "returns default pagination" do
+    context "when params are not present" do
+      it "returns default :length pagination" do
         service = described_class.new(Category.all, nil)
         result_categories = service.call
         expect(result_categories.count).to eq 10
       end
 
-      it "returns 10 records" do
+      it "returns first 10 records ordered by :created_at" do
         service = described_class.new(Category.all, nil)
         result_categories = service.call
         expected_categories = categories[0..9]
