26b1063aae08eba63e12048f3f58060392d47c97
Rian Dos Santos <riannegreiros@gmail.com>
2023-04-04 17:22:04 -0300

    create like searchable

--- api/app/controllers/concerns/name_searchable.rb
+++
@@ -1,7 +0,0 @@
-module NameSearchable
-  extend ActiveSupport::Concern
-
-  included do
-    scope :search_by_name, ->(name) { where('name LIKE ?', "%#{name}%") }
-  end
-end
--- api/spec/shared_examples/like_searchable_concern_example.rb
+++ api/spec/shared_examples/like_searchable_concern_example.rb
@@ -1,17 +1,17 @@
-shared_examples "name searchable concern" do |factory_name|
+shared_examples "like searchable concern" do |factory_name, field|
   let!(:search_params) { "Example" }
   let!(:records_to_find) do
-    (0..3).to_a.map { |index| create(factory_name, name: "Example #{index}") }
+    (0..3).to_a.map { |index| create(factory_name, field => "Example #{index}") }
   end
   let!(:records_to_ignore) { create_list(factory_name, 3) }
 
   it "returns only the records matching" do
-    found_records = described_class.search_by_name(search_params)
+    found_records = described_class.like(field, search_params)
     expect(found_records.to_a).to contain_exactly(*records_to_find)
   end
 
   it "does not return records that do not match" do
-    found_records = described_class.search_by_name(search_params)
+    found_records = described_class.like(field, search_params)
     expect(found_records.to_a).to_not include(*records_to_ignore)
   end
 end
