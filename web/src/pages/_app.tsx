581f9072048151e3e0500efc987a5bc3ed079e7d
Rian Dos Santos <riannegreiros@gmail.com>
2023-04-07 12:17:24 -0300

    config redux

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -3,6 +3,16 @@ import type { AppProps } from 'next/app'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from '@/store'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider>
+  )
 }
