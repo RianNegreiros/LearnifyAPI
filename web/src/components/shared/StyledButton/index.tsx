1898fa33359aa7f867b3e9ea5b8e21d27eb15064
Rian Dos Santos <riannegreiros@gmail.com>
2023-04-11 11:59:13 -0300

    add pagination

--- web/src/components/shared/StyledButton/index.tsx
+++ web/src/components/shared/StyledButton/index.tsx
@@ -2,16 +2,21 @@ import { Button } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { IconProp } from '@fortawesome/fontawesome-svg-core'
 import styles from './styles.module.css'
+import { ButtonHTMLAttributes } from 'react'
 
-interface StyledButtonProps {
-  icon?: IconProp,
-  action?: String,
+type StyledButtonProps =  ButtonHTMLAttributes<HTMLButtonElement> & {
+  icon?: IconProp
+  action?: string
+  active?: boolean
   type_button: string
 }
 
-export default function StyledButton({ icon, action, type_button }: StyledButtonProps) {
+export default function StyledButton({ icon, action, type_button, ...rest }: StyledButtonProps) {
   return (
-    <Button className={(type_button == "red") ? styles.red_button : styles.blue_button}>
+    <Button 
+    className={(type_button == "red") ? styles.red_button : styles.blue_button}
+    {...rest}
+    >
         { icon && <FontAwesomeIcon icon={icon} className={action && "mr-2"} /> } {action}
     </Button>
   )
