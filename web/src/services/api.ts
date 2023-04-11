d3f6483a7f92e68966588209bdc06a5c03306453
Rian Dos Santos <riannegreiros@gmail.com>
2023-04-11 09:52:42 -0300

    update api service

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,35 +1,70 @@
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 import Cookie from 'js-cookie'
 import ApiData from '@/dtos/ApiData'
+import ApiResponseError from '@/dtos/ApiResponseError'
+
+import Router from 'next/router'
+import { toast } from 'react-toastify'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+  baseURL: 'http://localhost:3000'
 })
 
-api.interceptors.response.use(res => {
-  if(res.headers['access-token']) {
+function setHeaders(res: AxiosResponse<any>) {
+  if(res.headers['access-token'] && res.headers['access-token'] !== '') {
     const apiData: ApiData = {
       'access-token': res.headers['access-token'],
       client: res.headers.client,
       expiry: res.headers.expiry,
       'token-type': res.headers['token-type'],
       uid: res.headers.uid
-    };
+    }
 
-    api.defaults.headers = apiData;
-    Cookie.set('@api-data', JSON.stringify(apiData));
+    api.defaults.headers = apiData
+    Cookie.set('@api-data', JSON.stringify(apiData))
+  }
+}
+
+api.interceptors.response.use(res => {
+  setHeaders(res)
+  return res
+}
+, err => {
+  if (err.response) {
+    setHeaders(err.response)
+
+    const data = err.response.data
+
+    if (data && data.errors && data.errors.fields) {
+      const errors = data.errors as ApiResponseError
+
+      const fieldsName = Object.keys(errors.fields)
+
+      fieldsName.forEach(error => {
+        toast.error(error + ': ' + errors.fields[error].join(`, `))
+      })
+
+      console.log('errors', errors)
+    }
   }
 
-  return res;
+  if (err.response && (
+      err.response.status === 401 ||
+      err.response.status === 403
+    )) {
+    Router.push('/Auth/Login')
+  }
+
+  throw err
 })
 
 api.interceptors.request.use(req => {
   if(req.url?.includes('admin')) {
-    const apiData: ApiData = JSON.parse(Cookie.get('@api-data') || '{}');
-    req.headers = apiData;
+    const apiData: ApiData = JSON.parse(Cookie.get('@api-data') || '{}')
+    req.headers = apiData
   }
 
-  return req;
+  return req
 })
 
 export default api
