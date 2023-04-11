bcbb190942637b7a95e5efe7ef646781c227ab4f
Rian Dos Santos <riannegreiros@gmail.com>
2023-04-11 12:36:19 -0300

    add categories list

--- web/src/components/shared/AdminDeleteModal/index.tsx
+++ web/src/components/shared/AdminDeleteModal/index.tsx
@@ -4,29 +4,29 @@ import styles from './styles.module.css'
 import StyledButton from '@/components/shared/StyledButton'
 
 interface AdminDeleteModalProps {
-    show: boolean,
-    handleClose: (success?: boolean) => void,
-    target: String
+  show: boolean,
+  handleClose: (success?: boolean) => void,
+  target: String
 }
 
 export default function AdminDeleteModal({ handleClose, show, target }: AdminDeleteModalProps) {
   return (
     <Modal show={show} onHide={handleClose} className={styles.modal} animation={true}>
       <Modal.Body className={styles.modal_body}>
-          Are sure you wanna delete {target}?
+        Are sure you wanna delete {target}?
 
-          <Row>
-            <Col lg={6} xs>
-                <div onClick={() => handleClose(true)}>
-                    <StyledButton icon={faTrash} action={"Excluir"} type_button="red" />
-                </div>
-            </Col>
+        <Row>
+          <Col lg={6} xs>
+            <div onClick={() => handleClose(true)}>
+              <StyledButton icon={faTrash} action={"Delete"} type_button="red" />
+            </div>
+          </Col>
 
-            <Col lg={6} xs>
-                <div onClick={() => handleClose(true)}>
-                    <StyledButton icon={faTimes} action={"Cancelar"} type_button="blue" />
-                </div>
-            </Col>
+          <Col lg={6} xs>
+            <div onClick={() => handleClose(true)}>
+              <StyledButton icon={faTimes} action={"Cancel"} type_button="blue" />
+            </div>
+          </Col>
         </Row>
       </Modal.Body>
     </Modal>
--- web/src/components/shared/TitleAdminPanel/index.tsx
+++ web/src/components/shared/TitleAdminPanel/index.tsx
@@ -4,30 +4,30 @@ import TitleAndPath from './TitleAndPath'
 import SearchAndIcon from './SearchAndIcon'
 import styles from './styles.module.css'
 
-
 interface TitleAdminPanelProps {
     title: String,
     path: String,
     icon?: IconProp
+    newPath: string
 }
 
-export default function TitleAdminPanel({ title, path, icon }: TitleAdminPanelProps) {
-  return (
-    <Row className="mt-4">
-    {
-        (icon) ?
-            <>
-                <Col lg={6} xs={4} className={styles.title_and_path}>
-                    <TitleAndPath title={title} path={path} />
-                </Col>
+export default function TitleAdminPanel({ title, path, icon, newPath }: TitleAdminPanelProps) {
+    return (
+        <Row className="mt-4">
+            {
+                (icon) ?
+                    <>
+                        <Col lg={6} xs={4} className={styles.title_and_path}>
+                            <TitleAndPath title={title} path={path} />
+                        </Col>
 
-                <Col lg={{span: 4, offset: 2}} xs={8}>
-                    <SearchAndIcon icon={icon} />
-                </Col>
-            </>
-        :
-            <TitleAndPath title={title} path={path} />
-    }
-    </Row>
-  )
+                        <Col lg={{ span: 4, offset: 2 }} xs={8}>
+                            <SearchAndIcon icon={icon} newPath={newPath} />
+                        </Col>
+                    </>
+                    :
+                    <TitleAndPath title={title} path={path} />
+            }
+        </Row>
+    )
 }
